30bccb3185c2fd32dafc682a39fe33f380a5209c
Radim Jaburek <pixiedevelopers@gmail.com>
2026-06-23 19:15:38 +0200

    basic verze

--- lib/main.dart
+++ lib/main.dart
@@ -15,13 +15,25 @@ class BikeTrackApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: const MainPage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = const [
+    _MapPage(),
+    _ProfilePage(),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -30,12 +42,43 @@ class HomePage extends StatelessWidget {
         title: const Text('BikeTrack'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'BikeTrack',
-          style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
-        ),
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            label: 'Mapa',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profil',
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class _MapPage extends StatelessWidget {
+  const _MapPage();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Mapa', style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold)),
+    );
+  }
+}
+
+class _ProfilePage extends StatelessWidget {
+  const _ProfilePage();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Profil', style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold)),
     );
   }
 }
